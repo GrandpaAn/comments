831f79c7b0ec6c2b262ff6fd6db6d326e0f7edfa
GrandpaAn <2236327498@qq.com>
2017-10-17 11:26:22 +0800

    second commit

---
+++ comments.php
@@ -0,0 +1,16 @@
+<div id="gitalk-container"></div>
+
+<link rel='stylesheet' href="https://yiyeti.cc/usr/themes/veryse/css/gitalk.css">
+<script src="https://yiyeti.cc/usr/themes/veryse/css/gitalk.min.js"></script>
+    <script>
+        var gitalk = new Gitalk({
+          clientID:  'bd4c8e823cfc5c4faa63',
+          clientSecret: '407fffb0ab272dfce16b339adc6c86a74f7908c5',
+          id: window.location.pathname,
+          repo: 'comments',
+          owner: 'GrandpaAn',
+          admin: '[GrandpaAn]',
+          distractionFreeMode: 'true',
+     })
+        gitalk.render('gitalk-container')
+</script>
